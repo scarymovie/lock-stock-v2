1d861018002f66360345b1cc7d155814147a3efd
evgeniy <evgeny.vinokurov@yoso.ru>
2025-01-01 16:30:32 +0300

    changed header

--- app/middleware/user.go
+++ app/middleware/user.go
@@ -15,9 +15,9 @@ const UserKey UserContextKey = "user"
 func UserAuthMiddleware(userFinder domain.UserFinder) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			userId := r.Header.Get("X-USER-ID")
+			userId := r.Header.Get("Authorization")
 			if userId == "" {
-				http.Error(w, "X-USER-ID header missing", http.StatusUnauthorized)
+				http.Error(w, "Authorization header missing", http.StatusUnauthorized)
 				return
 			}
 
