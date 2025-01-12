42881d9233578c01de8e53c54187e056f458e32b
evgeniy <evgeny.vinokurov@yoso.ru>
2025-01-13 00:42:35 +0300

    added cors for dev

--- app/router/router.go
+++ app/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"lock-stock-v2/external/domain"
 	"lock-stock-v2/external/handlers"
 	"lock-stock-v2/middleware"
@@ -16,6 +17,14 @@ func NewRouter(
 	createUser handlers.CreateUser,
 ) http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://172.24.0.2:8000"},
+		AllowedMethods:   []string{"POST"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	// Использование нескольких middleware.
 	r.With(
