d284b691591a4c5f8fe4fa2ba5d62b5f3b6dcc97
evgeniy <evgeny.vinokurov@yoso.ru>
2025-01-14 00:48:10 +0300

    more logs

--- app/middleware/logger.go
+++ app/middleware/logger.go
@@ -8,6 +8,14 @@ import (
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Request: %s %s\n", r.Method, r.URL.Path)
+
+		fmt.Println("Headers:")
+		for name, values := range r.Header {
+			for _, value := range values {
+				fmt.Printf("  %s: %s\n", name, value)
+			}
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
