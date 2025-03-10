f28d0ad6132786fa1e1625cc6196de3d283c2cae
evgeniy <evgeny.vinokurov@yoso.ru>
2025-03-10 23:40:44 +0300

    upd bet repo

--- app/internal/infrastructure/postgres/bet_repository.go
+++ app/internal/infrastructure/postgres/bet_repository.go
@@ -73,14 +73,26 @@ func (repo *BetRepository) Save(bet *model.Bet) error {
 
 func (repo *BetRepository) FindByRound(round *model.Round) ([]*model.Bet, error) {
 	query := `
-        SELECT
-            b.player_id,
-            b.amount,
-            b.number
-        FROM bets b
-        WHERE b.round_id = (SELECT id FROM rounds WHERE uid = $1)
-        ORDER BY b.number ASC
-    `
+		SELECT 
+			b.amount, 
+			b.number, 
+			p.balance, 
+			p.status, 
+			u.uid, 
+			u.name, 
+			g.uid, 
+			g.action_duration, 
+			g.question_duration, 
+			r.uid, 
+			r.status
+		FROM bets b
+		JOIN players p ON b.player_id = p.id
+		JOIN users u ON p.user_id = u.id
+		JOIN lock_stock_games g ON p.game_id = g.id
+		JOIN rooms r ON g.room_id = r.id
+		WHERE b.round_id = (SELECT id FROM rounds WHERE uid = $1)
+		ORDER BY b.number
+	`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -93,19 +105,35 @@ func (repo *BetRepository) FindByRound(round *model.Round) ([]*model.Bet, error)
 
 	var bets []*model.Bet
 	for rows.Next() {
-		var playerId uint
-		var amount int
-		var number uint
+		var (
+			amount           int
+			number           uint
+			balance          int
+			status           model.PlayerStatus
+			userUid          string
+			username         string
+			gameUid          string
+			actionDuration   string
+			questionDuration string
+			roomUid          string
+			roomStatus       roomModel.RoomStatus
+		)
 
-		err := rows.Scan(&playerId, &amount, &number)
+		err := rows.Scan(
+			&amount, &number,
+			&balance, &status,
+			&userUid, &username,
+			&gameUid, &actionDuration, &questionDuration,
+			&roomUid, &roomStatus,
+		)
 		if err != nil {
 			return nil, fmt.Errorf("failed to scan bet row: %w", err)
 		}
 
-		player, err := repo.findPlayerById(ctx, playerId)
-		if err != nil {
-			return nil, fmt.Errorf("failed to find player: %w", err)
-		}
+		user := userModel.NewUser(userUid, username)
+		room := roomModel.NewRoom(roomUid, roomStatus)
+		game := model.NewLockStockGame(gameUid, actionDuration, questionDuration, room)
+		player := model.NewPlayer(user, balance, status, game)
 
 		bet := model.NewBet(player, amount, round, number)
 		bets = append(bets, bet)
@@ -116,89 +144,4 @@ func (repo *BetRepository) FindByRound(round *model.Round) ([]*model.Bet, error)
 	}
 
 	return bets, nil
-}
-
-func (repo *BetRepository) findPlayerById(ctx context.Context, playerId uint) (*model.Player, error) {
-	var playerUid string
-	var userId uint
-	var balance int
-	var status model.PlayerStatus
-	var gameId uint
-
-	err := repo.db.QueryRow(ctx, `
-        SELECT p.uid, p.user_id, p.balance, p.status, p.game_id 
-        FROM players p 
-        WHERE p.id = $1`, playerId,
-	).Scan(&playerUid, &userId, &balance, &status, &gameId)
-	if err != nil {
-		return nil, fmt.Errorf("failed to find player: %w", err)
-	}
-
-	user, err := repo.findUserById(ctx, userId)
-	if err != nil {
-		return nil, fmt.Errorf("failed to find user: %w", err)
-	}
-
-	game, err := repo.findGameById(ctx, gameId)
-	if err != nil {
-		return nil, fmt.Errorf("failed to find game: %w", err)
-	}
-
-	return model.NewPlayer(user, balance, status, game), nil
-}
-
-func (repo *BetRepository) findUserById(ctx context.Context, userId uint) (*userModel.User, error) {
-	var uid string
-	var username string
-
-	err := repo.db.QueryRow(ctx, `
-        SELECT u.uid, u.name 
-        FROM users u 
-        WHERE u.id = $1`, userId,
-	).Scan(&uid, &username)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to find user: %w", err)
-	}
-
-	return userModel.NewUser(uid, username), nil
-}
-
-func (repo *BetRepository) findGameById(ctx context.Context, gameId uint) (*model.LockStockGame, error) {
-	var gameUid string
-	var actionDuration string
-	var questionDuration string
-	var roomId int
-
-	err := repo.db.QueryRow(ctx, `
-        SELECT uid, action_duration, question_duration, room_id
-        FROM lock_stock_games 
-        WHERE id = $1`, gameId,
-	).Scan(&gameUid, &actionDuration, &questionDuration, &roomId)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to find game: %w", err)
-	}
-
-	room := repo.findRoomById(ctx, uint(roomId))
-
-	return model.NewLockStockGame(gameUid, actionDuration, questionDuration, room), nil
-}
-
-func (repo *BetRepository) findRoomById(ctx context.Context, roomUd uint) *roomModel.Room {
-
-	var roomUid string
-	var roomStatus roomModel.RoomStatus
-
-	err := repo.db.QueryRow(ctx, `
-        SELECT uid, status
-        FROM rooms 
-        WHERE id = $1`, roomUd,
-	).Scan(&roomUid, roomStatus)
-
-	if err != nil {
-		return nil
-	}
-
-	return roomModel.NewRoom(roomUid, roomStatus)
 }
