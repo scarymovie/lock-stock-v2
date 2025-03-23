e7f0c97670fc0639038b72fc310c3e279eb51624
evgeniy <evgeny.vinokurov@yoso.ru>
2025-03-23 16:16:23 +0300

    upd chan

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"lock-stock-v2/wire"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -19,16 +23,31 @@ func main() {
 	port := ":8080"
 	fmt.Printf("Server is running on %s\n", port)
 	server := &http.Server{Addr: port, Handler: r}
-	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			fmt.Printf("Error starting server: %s\n", err)
-		}
-	}()
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("Error starting server: %s\n", err)
+			quit <- syscall.SIGTERM
+		}
+	}()
+
+	<-quit
 	fmt.Println("Shutting down server...")
-	server.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Error shutting down server: %v\n", err)
+	}
+
+	wg.Wait()
+	fmt.Println("Server exited.")
 }
